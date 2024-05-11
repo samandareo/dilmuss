05228a39eba933791715f84bc1c390a4d7a1c81e
Samandar <69756664+samandareo@users.noreply.github.com>
2024-05-11 21:00:36 +0500

    Update with_flask_api.py

--- with_flask_api.py
+++ with_flask_api.py
@@ -86,58 +86,41 @@ def index():
         '0a2b39c7-9774-4fe8-a226-f54012aa96f0'
     ]
 
-    oazis_sellers = [
-        '9d3b2c6f-d58f-4018-8a91-6d5fbd1a9251',
-        'ca610bd2-710b-4119-b4fb-943a2c2e1d5a',
-        '1cedf98b-3712-43c7-92f4-267d26d0f900',
-        '356f9378-f923-4838-aca8-0fe5eff99d06',
-        'e894b09a-7124-455a-b576-e7d1faa4e36c',
-        '1cf400f7-24ca-422d-9c0c-2785fc6d446d',
-        '6bcd38ae-076d-4031-98ed-0b93bcf7b863'
-    ]
-
     dilmuss_xavas = [entry for entry in data['seller_stats_by_date'] if entry['seller_id'] in xavas_sellers]
-    dilmuss_oazis = [entry for entry in data['seller_stats_by_date'] if entry['seller_id'] in oazis_sellers]
-
-    current_hour = datetime.now().hour
-    dilmuss_xavas_time = [entry for entry in dilmuss_xavas if datetime.strptime(entry['date'], '%Y-%m-%d %H:%M:%S').hour <= current_hour]
-    dilmuss_oazis_time = [entry for entry in dilmuss_oazis if datetime.strptime(entry['date'], '%Y-%m-%d %H:%M:%S').hour <= current_hour]
 
 
     xavas_team_plan = {
-        "XAVAS 1-KAMANDA .": 20000,
-        "XAVAS 2- KAMANDA .": 20000,
-        "XAVAS 3- KAMANDA .": 20000
+        "56c15498-18f0-4f24-b68f-f68cf790c086": 20000,
+        "613320af-4659-4e36-91ec-644350b39a81": 20000,
+        "10585d36-3f14-4a11-956c-846c9b386c97": 20000,
+        "1b46d22f-41c1-43c4-87cc-0174989949f1": 20000,
+        "d0f947af-1fae-4820-892a-d44d9c819a6d": 20000,
+        "8827e64b-af5a-4ae6-8ae4-8164b2e6e523": 20000,
+        "0a2b39c7-9774-4fe8-a226-f54012aa96f0": 20000
     }
-    default_plan = 20000  
 
     xavas_sales = {}
-    oazis_sales = {}
 
 
-    for entry in data['seller_stats_by_date']:
+    for entry in dilmuss_xavas:
         seller_name = entry['seller_name']
+        seller_id = entry['seller_id']
         net_gross_sales = entry['net_gross_sales']
-        if seller_name.startswith("XAVAS"):
-            plan = xavas_team_plan.get(seller_name, default_plan)
-            if seller_name in xavas_sales:
+        print(f'{seller_name} => {net_gross_sales}')
+
+        if seller_id in xavas_sellers:
+            if seller_name not in xavas_sales:
+                xavas_sales[seller_name] = {'total_sales': net_gross_sales, 'plan': xavas_team_plan[seller_id], 'percentage': 0}
+            else:
                 xavas_sales[seller_name]['total_sales'] += net_gross_sales/1000
-            else:
-                xavas_sales[seller_name] = {'total_sales': net_gross_sales, 'plan': plan, 'percentage': 0}
-            xavas_sales[seller_name]['percentage'] = (xavas_sales[seller_name]['total_sales'] / plan) * 100
-        else:
-            plan = default_plan
-            if seller_name in oazis_sales:
-                oazis_sales[seller_name]['total_sales'] += net_gross_sales/1000
-            else:
-                oazis_sales[seller_name] = {'total_sales': net_gross_sales, 'plan': plan, 'percentage': 0}
-            oazis_sales[seller_name]['percentage'] = (oazis_sales[seller_name]['total_sales'] / plan) * 100
+                print(f'${seller_name} => ${net_gross_sales}')
+            xavas_sales[seller_name]['percentage'] = (xavas_sales[seller_name]['total_sales'] / xavas_team_plan[seller_id]) * 100
+
 
   
     xavas_sales = dict(sorted(xavas_sales.items(), key=lambda item: item[1]['total_sales'], reverse=True))
-    oazis_sales = dict(sorted(oazis_sales.items(), key=lambda item: item[1]['total_sales'], reverse=True))
 
-    return render_template('index.html', oazis_sales=oazis_sales, xavas_sales=xavas_sales, current_time=interval_time)
+    return render_template('index.html', oazis_sales=xavas_sales, current_time=interval_time)
 
 
 def format_currency(value):
