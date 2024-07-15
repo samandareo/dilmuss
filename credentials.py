c02aace592108d468d762f3abdd42d42dd35b270
samandareo <smdeveloper04@gmail.com>
2024-07-15 17:44:20 +0500

    chore: Update credentials.py with new users and ids

--- credentials.py
+++ credentials.py
@@ -1,7 +1,13 @@
 users = {
     'oazis' : 'oazis1234',
     'xavas' : 'xavas1234',
-    'paris' : 'paris1234'
+    'paris' : 'paris1234',
+    'beruniy' : 'beruniy1234',
+    'nazira' : 'nazira1234',
+    'dressa' : 'dressa1234',
+    'muddi' : 'muddi1234',
+    'lady' : 'lady1234',
+    'instyle' : 'instyle1234'
 }
 
 ids = "a93a6aac-748b-4ee9-a4f3-7498cd1606f6%2C5a31a945-7567-437a-8d66-fd355260b6e8%2C92a24d88-be00-48bf-8935-bca2a10ca9bb%2C5d9949c0-eb71-49e1-888d-8784f106f8dd%2C611622c0-210b-4a64-88a6-b855be3d842d%2C198dbb62-12b6-45b2-8bba-fdf829994b50%2C0e0ec5e4-3ba0-4a73-82ae-7877f4789cf1%2Cfaf6343c-aeb4-4b44-9943-bcfade326b03%2Cb36214b2-ccc9-44ee-a5ab-550e8ce75212%2Ce927b1b4-f9b3-4fdd-8470-8e6a2f3c1365"
