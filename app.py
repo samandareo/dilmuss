486920f86caf784c6fbc93cd65ccf139ea8cf9a0
samandareo <smdeveloper04@gmail.com>
2024-07-15 15:59:07 +0500

    Update CSS files, added nazira branch

--- app.py
+++ app.py
@@ -300,7 +300,7 @@ def paris():
         global conn, cursor
         data = get_information("b36214b2-ccc9-44ee-a5ab-550e8ce75212")
         
-        cursor.execute("SELECT id FROM paris_nds")
+        cursor.execute("SELECT id FROM paris")
         db_information = cursor.fetchall()
         sellers = []
 
@@ -311,7 +311,7 @@ def paris():
     
         team_plan = {}
 
-        cursor.execute("SELECT * FROM paris_nds")
+        cursor.execute("SELECT * FROM paris")
         db_information = cursor.fetchall()
 
         for row in db_information:
@@ -425,7 +425,7 @@ def lady():
         global conn, cursor
         data = get_information("faf6343c-aeb4-4b44-9943-bcfade326b03")
         
-        cursor.execute("SELECT id FROM lady_house")
+        cursor.execute("SELECT id FROM lady")
         db_information = cursor.fetchall()
         sellers = []
 
@@ -436,7 +436,7 @@ def lady():
     
         team_plan = {}
 
-        cursor.execute("SELECT * FROM lady_house")
+        cursor.execute("SELECT * FROM lady")
         db_information = cursor.fetchall()
 
         for row in db_information:
@@ -481,6 +481,69 @@ def lady():
         return render_template('error.html', error_message=status, branch='lady')
 
 
+# --------------------- NAZIRA --------------------- #
+@app.route('/nazira')
+def nazira():
+    try:
+        global conn, cursor
+        data = get_information("611622c0-210b-4a64-88a6-b855be3d842d")
+        
+        cursor.execute("SELECT id FROM nazira")
+        db_information = cursor.fetchall()
+        sellers = []
+
+        for row in db_information:
+            sellers.append(row[0])
+
+        dilmuss = [entry for entry in data['seller_stats_by_date'] if entry['seller_id'] in sellers]
+    
+        team_plan = {}
+
+        cursor.execute("SELECT * FROM nazira")
+        db_information = cursor.fetchall()
+
+        for row in db_information:
+            team_plan[row[0]] = row[1]
+
+    
+        sales = {}
+    
+
+        for entry in dilmuss:
+            seller_id = entry['seller_id']
+            for row in db_information:
+                if seller_id == row[0]:
+                    seller_name = row[2]
+                    seller_position = row[3]
+                    seller_shift = row[4]
+                    break
+            net_gross_sales = entry['net_gross_sales']
+            
+            if interval_time.hour >= 17 and seller_shift == 2:
+                if seller_id in sellers:
+                    if seller_name not in sales:
+                        sales[seller_name] = {'total_sales': net_gross_sales, 'plan': team_plan[seller_id], 'percentage': 0}
+                    else:
+                        sales[seller_name]['total_sales'] += net_gross_sales/1000
+                    sales[seller_name]['percentage'] = (sales[seller_name]['total_sales'] / team_plan[seller_id]) * 100
+            elif interval_time.hour < 17 and (seller_shift == 1 or seller_shift == 0):
+                if seller_id in sellers:
+                    if seller_name not in sales:
+                        sales[seller_name] = {'total_sales': net_gross_sales, 'plan': team_plan[seller_id], 'percentage': 0}
+                    else:
+                        sales[seller_name]['total_sales'] += net_gross_sales/1000
+                    sales[seller_name]['percentage'] = (sales[seller_name]['total_sales'] / team_plan[seller_id]) * 100
+                    
+      
+        sales = dict(sorted(sales.items(), key=lambda item: item[1]['total_sales'], reverse=True))
+
+        return render_template('nazira.html', sales=sales,current_time=interval_time)
+    except Exception as e:
+        app.logger.error(e)
+        global status
+        return render_template('error.html', error_message=status, branch='nazira')
+
+# --------------------- Instyle --------------------- #
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     if request.method == 'POST':
