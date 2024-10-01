18c113a305e7665c21f1ec78a310db21381c8fac
Samandar <69756664+samandareo@users.noreply.github.com>
2024-10-01 16:20:02 +0500

    Update credentials.py

--- credentials.py
+++ credentials.py
@@ -10,6 +10,7 @@ users = {
     'instyle' : 'instyle1234'
 }
 
+# Below is just ids, just don't change anything
 ids = "a93a6aac-748b-4ee9-a4f3-7498cd1606f6%2C5a31a945-7567-437a-8d66-fd355260b6e8%2C92a24d88-be00-48bf-8935-bca2a10ca9bb%2C5d9949c0-eb71-49e1-888d-8784f106f8dd%2C611622c0-210b-4a64-88a6-b855be3d842d%2C198dbb62-12b6-45b2-8bba-fdf829994b50%2C0e0ec5e4-3ba0-4a73-82ae-7877f4789cf1%2Cfaf6343c-aeb4-4b44-9943-bcfade326b03%2Cb36214b2-ccc9-44ee-a5ab-550e8ce75212%2Ce927b1b4-f9b3-4fdd-8470-8e6a2f3c1365"
 shops = {
     'oazis' : 'a93a6aac-748b-4ee9-a4f3-7498cd1606f6',
